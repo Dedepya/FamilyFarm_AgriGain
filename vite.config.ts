8daeebe637e205b7fcbe6bdf284f437c3347c795
Dedeepya Yarlagadda <48832097+Dedepya@users.noreply.github.com>
2026-07-07 13:05:33 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
     plugins: [react(), tailwindcss()],
-    base: '/FamilyFarm_AgriGain/',
+    base: '/AGRI_GAIN-Portal/',
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
